1cb7fe9e6ff24c2007b92c950f08f39193e54b4f
nicholas <nnnnicholas@users.noreply.github.com>
2022-07-15 13:07:24 -0400

    added deadline

--- src/BannyBirthday.sol
+++ src/BannyBirthday.sol
@@ -7,13 +7,15 @@ contract BannyBirthday is ERC721 {
 
 uint256 public totalSupply;
 string metadata;
+        uint256 memory deadline;
 
     constructor(
         string memory _name,
         string memory _symbol,
         uint256 _projectId,
         address _beneficiary
-        string memory _metadata
+        string memory _metadata, 
+        uint256 memory _deadline // 1657972800 last time it'll be July 15 anywhere in the world
     )
         ERC721(_name, _symbol)
         JBETHERC20ProjectPayer(
@@ -26,10 +28,14 @@ string metadata;
             IJBDirectory(0xCc8f7a89d89c2AB3559f484E0C656423E979ac9C),
             address(this)
         )
-    {metadata = _metadata}
+    {
+        metadata = _metadata;
+        deadline = _deadline;
+    }
 
     function mint() external {
         require(msg.value>0.001, "must donate moar");
+        require(block.timestamp < deadline);
             _pay(
                 projectId, //uint256 _projectId,
                 JBTokens.ETH, // address _token
